3b099fa18edb0815b974c51065f4bc43125c0eb7
Maxim <cherviakovmaxim@gmail.com>
2022-07-26 19:59:33 +0300

    Some DB changes

--- SQLQuery1.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[Notes] (
-        [ID] [int] NOT NULL IDENTITY,
-        [COLOR] [nvarchar](max),
-        [CREATION_TIME] [nvarchar](max),
-        [NOTE] [nvarchar](max)
-    )
